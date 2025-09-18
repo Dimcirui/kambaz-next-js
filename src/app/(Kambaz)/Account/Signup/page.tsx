1aefcb1c593dad47d7e86c0548a39ace21137611
Dimcirui <dimcirui@qq.com>
2025-09-17 20:26:53 -0400

    update

--- src/app/(Kambaz)/Account/Signup/page.tsx
+++ src/app/(Kambaz)/Account/Signup/page.tsx
@@ -3,10 +3,10 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <input placeholder="username" className="wd-username" /><br/>
-      <input placeholder="password" type="password" className="wd-password" /><br/>
+      <input placeholder="username" className="wd-username" defaultValue={"Alice"}/><br/>
+      <input placeholder="password" type="password" className="wd-password" defaultValue={"Wonderland"}/><br/>
       <input placeholder="verify password"
-             type="password" className="wd-password-verify" /><br/>
+             type="password" className="wd-password-verify" defaultValue={"Wonderland"}/><br/>
       <Link  href="Profile" > Sign up </Link><br />
       <Link  href="Signin" > Sign in </Link>
     </div>
