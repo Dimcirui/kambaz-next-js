753c7a0f22fb1d9bd68056f9dc2faddbcd97c241
Dimcirui <dimcirui@qq.com>
2025-10-01 17:08:40 -0400

    Implement Lab2

--- src/app/(Kambaz)/Dashboard/page.tsx
+++ src/app/(Kambaz)/Dashboard/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Card, CardBody, CardImg, CardText, CardTitle, Button, Row, Col } from "react-bootstrap";
+
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
@@ -7,100 +9,127 @@ export default function Dashboard() {
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
-          <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.jpg" width={200} height={150} alt="React JS course cover" />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1235" className="wd-dashboard-course-link">
-            <Image src="/images/apcspcore.jpg" width={200} height={150} alt="APCSP course cover" />
-            <div>
-              <h5> CS1235 APCSP </h5>
-              <p className="wd-dashboard-course-title">
-                AP Computer Science Principles
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1236" className="wd-dashboard-course-link">
-            <Image src="/images/csintro.jpg" width={200} height={150} alt="CS Intro course cover" />
-            <div>
-              <h5> CS1236 CS Intro </h5>
-              <p className="wd-dashboard-course-title">
-                Computer Science Introduction
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1237" className="wd-dashboard-course-link">
-            <Image src="/images/cybersecurity.jpg" width={200} height={150} alt="Cybersecurity course cover" />
-            <div>
-              <h5> CS1237 Cybersecurity </h5>
-              <p className="wd-dashboard-course-title">
-                Cybersecurity fundamentals
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1238" className="wd-dashboard-course-link">
-            <Image src="/images/fullstack.jpg" width={200} height={150} alt="Full Stack Web Development course cover" />
-            <div>
-              <h5> CS1238 Full Stack Web Development </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack web application developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1239" className="wd-dashboard-course-link">
-            <Image src="/images/machinelearning.jpg" width={200} height={150} alt="Machine Learning course cover" />
-            <div>
-              <h5> CS1239 Machine Learning </h5>
-              <p className="wd-dashboard-course-title">
-                Machine Learning basics
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1240" className="wd-dashboard-course-link">
-            <Image src="/images/newmedia.jpg" width={200} height={150} alt="New Media course cover" />
-            <div>
-              <h5> CS1240 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/Courses/1241" className="wd-dashboard-course-link">
-            <Image src="/images/datascience.jpg" width={200} height={150} alt="Data Science course cover" />
-            <div>
-              <h5> CS1241 New Media design </h5>
-              <p className="wd-dashboard-course-title">
-                New Media design principles
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
+          <Row xs={1} md={5} className="g-4">
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1234/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/reactjs.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1234 React JS</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Full Stack software developer</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1235/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/apcspcore.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1235 APCSP</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  AP Computer Science Principles</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1236/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/csintro.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1236 CS Intro</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Computer Science Introduction</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1237/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/cybersecurity.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1237 Cybersecurity</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Cybersecurity fundamentals</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1238/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/fullstack.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1238 Full Stack Web Development</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Full Stack web application developer</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1239/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/machinelearning.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1239 Machine Learning</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Machine Learning basics</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1240/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/newmedia.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1240 React JS</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  Full Stack software developer</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+
+            <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+              <Card>
+              <Link href="/Courses/1241/Home"
+                    className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/datascience.jpg" width="100%" height={160}/>
+                <CardBody>
+                <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1241 New Media design</CardTitle>
+                <CardText  className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                  AP Computer Science Principles</CardText>
+                <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+              </Card>
+            </Col>
+          </Row>
         </div>
       </div>
     </div>
--- src/app/Labs/TOC.tsx
+++ src/app/Labs/TOC.tsx
@@ -1,28 +1,31 @@
+import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
 export default function TOC() {
  return (
-   <ul>
-     <li>
-       <Link href="/Labs" id="wd-lab1-link">
-         Home </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab1" id="wd-lab1-link">
-         Lab 1 </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab2" id="wd-lab2-link">
-         Lab 2 </Link>
-     </li>
-     <li>
-       <Link href="/Labs/Lab3" id="wd-lab3-link">
-         Lab 3 </Link>
-     </li>
-     <li>
-       <Link href="/" id="wd-kambaz-link">
-         Kambaz </Link> </li>
-     <li>
-       <Link href="https://github.com/Dimcirui/kambaz-next-js" id="wd-github">
-         Github</Link> </li>
-   </ul>
+   <Nav variant="pills">
+     <NavItem>
+       <NavLink href="/Labs" id="wd-lab1-link">
+         Home </NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Labs/Lab1" id="wd-lab1-link">
+         Lab 1 </NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Labs/Lab2" id="wd-lab2-link">
+         Lab 2 </NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Labs/Lab3" id="wd-lab3-link">
+         Lab 3 </NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/" id="wd-kambaz-link">
+         Kambaz </NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="https://github.com/Dimcirui/kambaz-next-js" id="wd-github">
+         Github</NavLink>
+     </NavItem>
+   </Nav>
 );}
