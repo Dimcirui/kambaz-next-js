5ea78250b902cae89778851dcbd4b78ebe4989de
Dimcirui <dimcirui@qq.com>
2025-11-13 00:40:03 -0500

    bug fix

--- src/app/(Kambaz)/Dashboard/page.tsx
+++ src/app/(Kambaz)/Dashboard/page.tsx
@@ -26,7 +26,7 @@ export default function Dashboard() {
   });
   const fetchCourses = async () => {
     try {
-      const courses = await client.findMyCourses();
+      const courses = await client.fetchAllCourses();
       dispatch(setCourses(courses));
     } catch (error) {
       console.error(error);
